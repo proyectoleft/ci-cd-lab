4fee7f1d1c7a93633da31174a181675f9c7523f9
proyectoleft <diego.cardenas.t@uni.pe>
2025-06-21 00:05:48 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ import app
 
 class TestApp(unittest.TestCase):
     def test_output(self):
-        self.assertIsNone(app.main())  # Verifica que main() no retorne nada
+        self.assertEqual(app.main(), "Hola desde la app de CI/CD")
 
 if __name__ == '__main__':
     unittest.main()
